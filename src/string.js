1f61e6bdb06b463965bb020c10289f157ebb4f22
Rahul <rahulcy7@gmail.com>
2025-09-13 11:09:10 +0530

    feat: single/multiple delimiter in string

--- src/string.js
+++ src/string.js
@@ -1,4 +1,24 @@
-const string = (numbers) =>
-  numbers.split(",").reduce((a, b) => a + Number(b), 0);
+const validString = (numbers) => {
+  if (numbers === "") return true;
+  const regex = /^[0-9]+(,[0-9]+)*$/;
+  return regex.test(numbers);
+};
+
+const addStringNumbers = (numbers) => {
+  return numbers.split(",").reduce((a, b) => a + Number(b), 0);
+};
+
+const normalizeString = (numbers) => {
+  return numbers.replace(/\n/g, ",");
+};
+
+const string = (numbers) => {
+  if (validString(numbers)) {
+    return addStringNumbers(numbers);
+  } else {
+    const normalNumbers = normalizeString(numbers);
+    return addStringNumbers(normalNumbers);
+  }
+};
 
 export default string;
--- tests/string.test.js
+++ tests/string.test.js
@@ -7,3 +7,11 @@ test("empty string", () => {
 test("any amount of numbers", () => {
   expect(string("1,2,3,4,5,6,7,8,9")).toBe(45);
 });
+
+test("numbers string with one delimiter", () => {
+  expect(string("1,2,3,4,5,6\n7,8,9")).toBe(45);
+});
+
+test("numbers string with multiple new line delimiter", () => {
+  expect(string("1,2\n3,4,5,6\n7,8,9")).toBe(45);
+});
